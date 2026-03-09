9e45ab7b20b220b307183f86fac700dfddd43dee
Rokeshwaran <rokeshwaran2008@gmail.com>
2026-03-09 22:07:08 +0530

    Time: N/A (0%), Space: N/A (0%)

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,17 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        char val = t.charAt(0);
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+        for(char ch : t.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+            if(map.get(ch)==1){
+                val = ch;
+                break;
+            }
+        }
+        return val;
+    }
+}
