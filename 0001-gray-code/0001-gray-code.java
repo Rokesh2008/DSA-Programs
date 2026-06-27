7a93825c9e29d83e293b05f2efedaa31dca5eb46
Rokeshwaran <rokeshwaran2008@gmail.com>
2026-06-27 23:07:47 +0530

    Time: 5 ms (60.34%), Space: 53.6 MB (73.51%)

---
+++ 0001-gray-code/0001-gray-code.java
@@ -0,0 +1,9 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ls = new ArrayList<>();
+        for(int i =0;i<(1<<n);i++){
+            ls.add(i^(i>>1));
+        }
+        return ls;
+    }
+}
