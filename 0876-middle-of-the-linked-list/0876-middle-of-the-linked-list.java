798e1260361cf62f84dc4f1db6d6e69e6b177604
Rokeshwaran <rokeshwaran2008@gmail.com>
2026-07-08 10:41:10 +0530

    Time: 0 ms (100%), Space: 43 MB (35.46%)

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode temp = head;
+        int count = 0;
+
+        while(temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        int mid = count / 2 + 1;
+        temp = head;
+
+        while(temp != null) {
+            mid--;
+
+            if(mid == 0) break;
+
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+}
