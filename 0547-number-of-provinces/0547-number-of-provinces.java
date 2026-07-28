dbb44aebfd79a414e9b57d6e2b8f08682ae4263c
Rokeshwaran <rokeshwaran2008@gmail.com>
2026-07-28 11:31:09 +0530

    Time: 1 ms (86.93%), Space: 47.4 MB (44.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,22 +1,57 @@
-class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        Set<Integer> visited = new HashSet<>();
-        int provinces = 0;
-        for (int i = 0; i < isConnected.length; i++) {
-            if (!visited.contains(i)) {
-                dfs(i, isConnected, visited);
-                provinces++;
+class Solution{
+    class disjoin{
+        int[] leader;
+        int[] size;
+        disjoin(int n){
+            leader = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++){
+                leader[i] =i;
+                size[i] =1;
             }
         }
-        return provinces;        
+        void union(int u, int v){
+            int pu =find(u);
+            int pv =find(v);
+            if (pu ==pv){
+                return;
+            }
+            if (size[pu]<size[pv]){
+                leader[pu] = pv;
+                size[pv]+= size[pu];
+            } 
+            else{
+                leader[pv] =pu;
+                size[pu] +=size[pv];
+            }
+        }
+        int find(int n){
+            if(leader[n] == n){
+                return n;
+
+            }
+            return leader[n]=find(leader[n]);
+        }
+
     }
-    private void dfs(int city, int[][] isConnected, Set<Integer> visited) {
-        visited.add(city);
-        for (int cur = 0; cur < isConnected[city].length; cur++) {
-            int connected = isConnected[city][cur];
-            if (connected == 1 && !visited.contains(cur)) {
-                dfs(cur, isConnected, visited);
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        disjoin dis = new disjoin(n);
+        for (int i = 0;i < n; i++){
+            for (int j =i;j < n; j++){
+                if (isConnected[i][j] == 1){
+                    dis.union(i, j);
+
+                }
             }
         }
-    }    
+        int provi = 0;
+        for(int i = 0; i<n; i++){
+            if(dis.find(i) == i){
+                provi++;
+            }
+        }
+        return provi;
+
+    }
 }
