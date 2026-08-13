5348872f8d5a8d9e3861f88726fbdda77641ad0b
Rokeshwaran <rokeshwaran2008@gmail.com>
2026-08-13 23:59:11 +0530

    Time: 0 ms (100%), Space: 43.7 MB (13.96%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,16 +1,27 @@
 class Solution {
-    public boolean rotateString(String s, String goal) {
-        int len = s.length();
-        int x = len;
-        while(x-->0){
-            StringBuilder sb = new StringBuilder();
-            sb.append(s.charAt(len-1));
-            for(int i = 0; i<len-1; i++){
-                sb.append(s.charAt(i));
+    public boolean rotateString(String A, String B) {
+        if(A == null || B == null){
+            return false;
+        }
+        if(A.length() != B.length()) {
+            return false;
+        }
+        if(A.length() == 0) {
+            return true;
+        }
+        for(int i = 0; i < A.length(); i++) {
+            if(rotateString(A, B, i)) {
+                return true;
             }
-            if(sb.toString().equals(goal))return true;
-            s = sb.toString();
         }
         return false;
+    }
+    private boolean rotateString(String A, String B, int rotation) {
+        for(int i = 0; i < A.length(); i++) {
+            if(A.charAt(i) != B.charAt((i+rotation)%B.length())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
