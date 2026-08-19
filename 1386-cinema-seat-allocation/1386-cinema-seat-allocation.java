1386e760b121c5a81ff12d4a51dfa869f356aea9
Rokeshwaran <rokeshwaran2008@gmail.com>
2026-08-19 09:12:29 +0530

    Time: 17 ms (91.32%), Space: 52.7 MB (48.16%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,20 +1,27 @@
-class Solution {
-    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        Map<Integer,Set<Integer>> map=new HashMap<>();
-        for(int[] seat:reservedSeats){
-            map.putIfAbsent(seat[0],new HashSet<>());
-            map.get(seat[0]).add(seat[1]);
-        }
-        int ans=(n-map.size())*2;
-        for(Set<Integer> set:map.values()){
-            boolean left=!set.contains(2) && !set.contains(3) && !set.contains(4) && !set.contains(5);
-            boolean middle=!set.contains(4) && !set.contains(5) && !set.contains(6) && !set.contains(7);
-            boolean right=!set.contains(6) && !set.contains(7) && !set.contains(8) && !set.contains(9);
-            if(left && right){
-                ans+=2;
+import java.util.*;
+class Solution{
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] a : reservedSeats){
+            int row = a[0];
+            int seat = a[1];
+            if ((seat>=2) && (seat<=9)){
+                map.put(row, map.getOrDefault(row, 0) | (1 << (seat - 2)));
             }
-            else if(left || middle || right){
-                ans+=1;
+        }
+        int left = 15;
+        int middle = 60;
+        int right = 240;
+        int ans = (n -map.size())*2;
+        for (int mask : map.values()){
+            boolean l = (mask & left) ==0;
+            boolean m = (mask & middle) == 0;
+            boolean r = (mask & right) == 0;
+            if(l&&r){
+                ans += 2;
+            } 
+            else if(l||m||r){
+                ans++;
             }
         }
         return ans;
